70f04b78fbd6713949f73eedb128483add1380c4
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-06-15 20:55:12 -0300

    feat: adiciona exercício carro poo em python

---
+++ Linguagens-de-POO-IFSUL/Python-POO/POO/Carro/Carro.py
@@ -0,0 +1,5 @@
+class Carro():
+    def __init__(self, nome, marca, ano):
+        self.nome = nome
+        self.marca = marca
+        self.ano = ano
---
+++ Linguagens-de-POO-IFSUL/Python-POO/POO/Carro/Exec.py
@@ -0,0 +1,6 @@
+import Carro as car
+
+c1 = car.Carro('Polo', 'Volkswagen', 2021)
+print('Carro:', c1.nome)
+print('Marca:', c1.marca)
+print('Ano:', c1.ano)
