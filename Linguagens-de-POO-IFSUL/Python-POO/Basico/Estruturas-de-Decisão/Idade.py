fdbb1f225f1d7992d910ab8c6bc0397fb1c63b7d
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-06-15 16:10:45 -0300

    feat: adiciona basico python

---
+++ Linguagens-de-POO-IFSUL/Python-POO/Basico/Estruturas-de-Decisão/Idade.py
@@ -0,0 +1,10 @@
+idade = 18
+
+if idade < 12:
+    print('Criança')
+elif idade < 18:
+    print('Adolecente')
+elif idade < 70:
+    print('Adulto')
+else:
+    print('Idoso')
---
+++ Linguagens-de-POO-IFSUL/Python-POO/Basico/Estruturas-de-Repeticao/EstruturasRepeticao.py
@@ -0,0 +1,17 @@
+cont = 0
+
+while (cont < 5):
+    print(cont)
+    cont = cont + 1
+else:
+    print('Fim da execução')
+
+nomes = ['Maria', 'Pedro', 'Guilherme', 'Laura', 'Bia']
+
+for n in nomes:
+    print(n)
+else:
+    print("Todos os nomes foram listados")
+
+
+    
