c9f2930f32a40ec43fa1e539feb8a8668a23fc16
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-06-18 18:22:22 -0300

    feat: cria a estrutura do bd livraria_cia

---
+++ Descomplicando-Banco-de-Dados-IFSP/livraria_cia.sql
@@ -0,0 +1,39 @@
+use livraria_cia;
+/*
+CREATE TABLE clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE autores (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    nacionalidade VARCHAR(50)
+);
+
+CREATE TABLE livros (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(150),
+    preco DECIMAL(10,2),
+    autor_id INT,
+    FOREIGN KEY (autor_id) REFERENCES autores(id_autor)
+);
+
+CREATE TABLE pedidos (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    data DATE,
+    cliente_id INT,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE itens_pedido (
+    id_item INT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id INT,
+    livro_id INT,
+    quantidade INT,
+    FOREIGN KEY (pedido_id) REFERENCES pedidos(id_pedido),
+    FOREIGN KEY (livro_id) REFERENCES livros(id_livro)
+);
+*/
