e6ba76e2b942c33b3927ef6c969ab9b73d6cafe3
Leonardo Serqueira <leonardoserqueira@hotmail.com>
2026-06-19 00:31:30 -0300

    feat: adiciona tabelas e colunas(FKS) em tabelas existentes, tambem popula o banco.

--- Descomplicando-Banco-de-Dados-IFSP/livraria_cia.sql
+++ Descomplicando-Banco-de-Dados-IFSP/livraria_cia.sql
@@ -1,5 +1,7 @@
 use livraria_cia;
 
+
+
 /*
 CREATE TABLE clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
@@ -38,7 +40,61 @@ CREATE TABLE itens_pedido (
     FOREIGN KEY (livro_id) REFERENCES livros(id_livro)
 );
 
-
+-- altera a tabela pedidos para adicionar coluna valor_total
 alter table pedidos add column valor_total decimal(10,2);
+
+insert into autores(nome, nacionalidade)
+values('J. K. ROWLING', 'Britânica'), ('George R. R. Martin', 'Americana'),
+('Machado de Assis', 'Brasileira');
+
+
+-- cria a tabela categoria
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nome_categoria VARCHAR(150)
+)
+
+INSERT INTO categoria (nome_categoria) 
+VALUES ('Ficção'), ('Fantasia'), ('Drama');
+
+
+-- cria a tabela fornecedor
+CREATE TABLE fornecedor (
+    id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+    nome_fornecedor VARCHAR(100),
+    telefone VARCHAR(20)
+);
+
+INSERT INTO fornecedor 
+(nome_fornecedor, telefone)
+VALUES
+('Distribuidora Alfa', '1199999-0000'),
+('Editora Beta', '1198888-1234');
+
+
+-- altera a tabela livros para adicionar colunas de id
+ALTER TABLE livros
+ADD categoria_id INT,
+ADD fornecedor_id INT,
+ADD FOREIGN KEY (categoria_id) REFERENCES categoria(id_categoria),
+ADD FOREIGN KEY (fornecedor_id) REFERENCES fornecedor(id_fornecedor);
+
+INSERT INTO livros (titulo, preco, autor_id, id_categoria, id_fornecedor)
+VALUES
+('Dom Casmurro', 39.90, 3, 3, 1),
+('A Guerra dos Tronos', 59.90, 2, 2, 2),
+('Harry Potter e a Pedra Filosofal', 49.90, 1, 2, 1);
+
+-- select com inner join triplo e com com siglas nas tabelas(Table Aliases)
+SELECT
+	l.id_livro, l.titulo,
+	c.id_categoria, c.nome_categoria,
+    a.id_autor, a.nome, a.nacionalidade
+FROM livros l 
+INNER JOIN categoria c ON l.categoria_id = c.id_categoria
+INNER JOIN autores a ON l.autor_id = a.id_autor 
+WHERE l.categoria_id = 2 
+ORDER BY l.id_livro;
+
 
 */
